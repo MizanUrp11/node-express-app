2c0bd77bc2c6bd00df9d5909adab5d1be88633c4
MizanUrp11 <mrn1105009@gmail.com>
2021-03-22 20:04:24 +0600

    Creating Page Routes

--- index.js
+++ index.js
@@ -10,13 +10,16 @@ app.get('/',(req,res)=>{
     res.sendFile(path.resolve(__dirname,'public','index.html'));
 })
 app.get('/about',(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'about.html'));
+    res.sendFile(path.resolve(__dirname, 'public','about.html'));
+})
+app.get('/post',(req,res)=>{
+    res.sendFile(path.resolve(__dirname, 'public','post.html'));
 })
 app.get('/contact',(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'contact.html'));
+    res.sendFile(path.resolve(__dirname,'public','contact.html'));
 })
 app.get('*',(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'404.html'))
+    res.sendFile(path.resolve(__dirname,'public','404.html'))
 })
 app.listen(port,()=>{
     console.log(`app is running at port: ${port}`);
