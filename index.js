f2d313f43086039932b58d32df32b01a7c186751
MizanUrp11 <mrn1105009@gmail.com>
2021-03-22 20:00:22 +0600

    Bootstrapping new theme

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ const app = express();
 const path = require("path");
 const port = 3000;
 
+///////////Middlewares////////
+app.use(express.static('public'));
+
 app.get('/',(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'index.html'));
+    res.sendFile(path.resolve(__dirname,'public','index.html'));
 })
 app.get('/about',(req,res)=>{
     res.sendFile(path.resolve(__dirname,'about.html'));
@@ -13,7 +16,7 @@ app.get('/contact',(req,res)=>{
     res.sendFile(path.resolve(__dirname,'contact.html'));
 })
 app.get('*',(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'404.html'));
+    res.sendFile(path.resolve(__dirname,'404.html'))
 })
 app.listen(port,()=>{
     console.log(`app is running at port: ${port}`);
