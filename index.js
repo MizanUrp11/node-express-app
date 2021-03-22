0531b7a6df1b59b1febe41b405b013ab88b635ed
MizanUrp11 <mrn1105009@gmail.com>
2021-03-22 19:08:45 +0600

    Basic Request and Response

---
+++ index.js
@@ -0,0 +1,14 @@
+const http = require("http");
+const server = http.createServer((req,res)=>{
+    if(req.url == "/about")
+        res.end("About page");
+    else if(req.url == "/contact")
+        res.end("contact page");
+    else if(req.url == "/")
+        res.end("Home page");
+    else{
+        res.writeHead(404);
+        res.end("Page not found");
+    }
+})
+server.listen(3000);
