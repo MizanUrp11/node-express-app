7606f1ff894af4bae3b4712e62cedd26f0e7b59c
MizanUrp11 <mrn1105009@gmail.com>
2021-03-28 08:27:21 +0600

    Displaying Validation Errors in Template

--- controllers/storeUserController.js
+++ controllers/storeUserController.js
@@ -4,6 +4,8 @@ module.exports = (req,res)=>{
     User.create(req.body,(error,user)=>{
         if(error){
             console.log('username already taken');
+            const validationErrors = Object.keys(error.errors).map(key=>error.errors[key].message);
+            req.session.validationErrors = validationErrors;
             return res.redirect('/auth/register');
         }
         console.log(error,user);
