3d5ef0354f5030ab39ec906a46f1de539f51fa55
MizanUrp11 <mrn1105009@gmail.com>
2021-03-30 17:18:52 +0600

    Post page fix

--- controllers/getPostController.js
+++ controllers/getPostController.js
@@ -2,8 +2,8 @@ const BlogPost = require("../models/BlogPost")
 
 module.exports = async (req, res) => {
     await BlogPost.findById(req.params.id, (error, blogpost) => {
-        res.render('post', {
+        res.render('post',{
             blogpost
         })
-    })
+    }).populate('userId');
 }
